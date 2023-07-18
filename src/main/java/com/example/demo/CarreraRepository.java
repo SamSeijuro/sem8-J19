d764b1143b1cc7bd56a90c053b676ca27f3efa70
SamSeijuro <135294606+SamSeijuro@users.noreply.github.com>
2023-07-18 02:31:57 +0000

    Commit Repository

--- src/main/java/com/example/demo/CarreraRepository.java
+++ src/main/java/com/example/demo/CarreraRepository.java
@@ -11,5 +11,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "carreras", path = "carreras")
 public interface CarreraRepository extends PagingAndSortingRepository<Carrera, Integer>, CrudRepository<Carrera, Integer> {
     
-    List<Curso> findByNombre(@Param("nombre") String nombre);
+    List<Carrera> findByNombre(@Param("nombre") String nombre);
 }
